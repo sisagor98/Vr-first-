bd0c1e4fe69b46cc8d96624821f4d556953fea58
shariful islam <sisagor98@gmail.com>
2020-02-28 11:40:12 +0600

    first commit

---
+++ Assets/Mywork/Script/Gun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range =100f;
+    
+    public Camera fpsCam;
+    public ParticleSystem mazzleflase;
+
+  
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+
+    }
+
+    void Shoot(){
+        mazzleflase.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log("Fire");
+        }
+    }
+
+
+}
